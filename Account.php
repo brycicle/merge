299952bb9a42ef62fdfb2077822b205ddd409df4
Bryce Deyto <bryce.deyto@gmail.com>
2023-10-05 21:25:57 +0800

    removed space

--- Account.php
+++ Account.php
@@ -4,18 +4,14 @@
 	include "includes/Header.php";
 	include "includes/Navbar.php";
 	include "includes/Topbar.php";
-
-
 ?>
 <!DOCTYPE html>
 <html>
-
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 </head>
-
 <style type="text/css">
 	body {
 	    margin: 0;
