c4dcd538c7fc07e473cc43b66b2f436abb4aad70
peterpaul123 <peterpaulsandiego25@gmail.com>
2023-10-07 20:44:57 +0800

    sign in and sign up topbar edit

--- Header1.php
+++ Header1.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- Site Metas -->
-	<title>M.E.R.G.E</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Site Metas -->
+    <title>M.E.R.G.E</title>
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -13,12 +13,12 @@
     <!-- Css -->
     <link rel="stylesheet" type="text/css" href="#">
     <!-- Bootstrap -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- Boxicons CSS -->
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <!-- Alertify Js -->
-	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
-	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css"/>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css"/>
 </head>
 <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap');
@@ -57,14 +57,21 @@
             text-transform: uppercase;
             transition: 0.6s;
         }
-        header .signup,
-        .lead {
-            position: relative;
+        header .signup{
+            margin-left: 42%;
+            position: absolute;
+            text-decoration: none;
+            color: #fff;
+        }
+        header .lead {
+            margin-left: 50%;
+            position: absolute;
             text-decoration: none;
             color: #fff;
         }
         header .signin {
-            position: relative;
+            margin-left: 54%;
+            position: absolute;
             text-decoration: none;
             color: #fff;
         }
@@ -113,6 +120,25 @@
             header.sticky ul li a {
                 margin: 10px 0;
             }
+            header .signup,
+            .lead {
+                
+                text-decoration: none;
+                color: #fff;
+            }
+            header .signin {
+                
+                text-decoration: none;
+                color: #fff;
+            }
+        }
+        /* Add a media query for larger screens where you want to remove spacing */
+        @media (min-width: 769px) {
+            header .signup,
+            header .lead,
+            header .signin {
+                margin-right: 10px;
+            }
         }
         .logo:hover {
             color: #fff;
@@ -127,11 +153,11 @@
         }
     </style>
 <body>
-	<header>
-		<a href="index.php" class="logo"><img style="width: 150px;" src="img/Logolabel2.png"></a>
+    <header>
+        <a href="index.php" class="logo"><img style="width: 150px;" src="img/Logolabel2.png"></a>
         <a href="Signup.php" class="signup">Sign Up</a><p class="lead">|</p><a href="Signin.php" class="signin">Sign In</a>
-	</header>
-	<script>
+    </header>
+    <script>
         window.addEventListener('scroll', function() {
             const header = document.querySelector('header');
             const logoImg = document.querySelector('.logo img');
